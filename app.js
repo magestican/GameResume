1fc757e127e897354cd8b7b1d6486689cd1745e9
unknown <magestican.visualkei@gmail.com>
2014-11-28 20:58:33 +0900

    HEROKU CHANGES

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ var path = require('path');
 var app = express();
 
 app.configure(function () {
-    var port = process.env.PORT || 443
-    process.env.PWD = process.cwd()
+    var port = process.env.PORT || 3000
+    console.log("LOOK AT THIS")
+    console.log(__dirname);
     app.set('port', port);
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
-    app.use('build', express.static(__dirname + 'build'));
+    app.use(express.static(path.join(__dirname, 'build')));
     app.use(express.static(path.join(__dirname, 'assets')));
 });
 
@@ -24,4 +25,9 @@ var options = {
 
 https.createServer(options, app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.get('/', function (req, res) {
+    console.log("something");
+    res.send()
 });
