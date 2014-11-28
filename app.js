4a553961df785f7ec0367daf45d27bd27ee94356
unknown <magestican.visualkei@gmail.com>
2014-11-28 20:06:01 +0900

    heroku changes

--- app.js
+++ app.js
@@ -7,24 +7,25 @@ var path = require('path');
 var app = express();
 
 app.configure(function () {
-  app.set('port', 443);
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'build')));
-  app.use(express.static(path.join(__dirname, 'assets')));
+    var port = process.env.PORT || 443
+    app.set('port', 443);
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static(path.join(__dirname, 'assets')));
 });
 
 var options = {
-  key: fs.readFileSync('ssl/server.key'),
-  cert: fs.readFileSync('ssl/server.crt')
+    key: fs.readFileSync('ssl/server.key'),
+    cert: fs.readFileSync('ssl/server.crt')
 };
 
 https.createServer(options, app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
 
 app.get('/', function (req, res) {
     console.log("something");
-  res.send()
+    res.send()
 });
